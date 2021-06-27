65e66938225afe9e2690cb191f2b7e941fcc66ac
sKawashima <bysk.333@gmail.com>
2021-06-27 14:40:32 +0900

    Make minimum style

--- app/articles/components/ArticleForm.tsx
+++ app/articles/components/ArticleForm.tsx
@@ -2,11 +2,17 @@ import { Form, FormProps } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { z } from "zod"
 export { FORM_ERROR } from "app/core/components/Form"
+import { Field } from "react-final-form"
+import styles from "./index.module.sass"
 
 export function ArticleForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
-      <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <LabeledTextField name="title" label="Title" placeholder="title" />
+      <div className="labeledField">
+        <label className={styles.label}>body</label>
+        <Field name="body" component="textarea" placeholder="body" />
+      </div>
     </Form>
   )
 }
